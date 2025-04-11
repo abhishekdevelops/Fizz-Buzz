8d8dfd8485f31b1a921475d602a5965b1fdec7cf
abhishek-web-design <abhishekvadnala10@gmail.com>
2025-04-11 13:52:10 +0530

    Initial commit - Fizz-Buzz

---
+++ script.js
@@ -0,0 +1,13 @@
+let n = 1;
+
+for (let i = n; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
